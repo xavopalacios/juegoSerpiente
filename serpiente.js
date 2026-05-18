e3a67c754e07d44ccb79964f1a13e059e34c79fa
xavopalacios <xavopalacios@gmail.com>
2026-05-17 21:44:58 -0500

    feat: implementa pintura de la serpiente con cabeza diferenciada

--- serpiente.js
+++ serpiente.js
@@ -2,27 +2,30 @@
 const canvas = document.getElementById("canvasJuego");
 const ctx = canvas.getContext("2d");
 
-// PASO 4: Constante para el tamaño de cada celda de la cuadrícula
+// CONSTANTES BASE
 const TAMANIO_CELDA = 25;
 
-// Primera pintura del juego al cargar la página
+
+const serpiente = [
+  { x: 12, y: 12 }, // Cabeza
+  { x: 11, y: 12 }, // Cuerpo parte 1
+  { x: 10, y: 12 }, // Cuerpo parte 2
+  { x: 9, y: 12 }   // Cuerpo parte 3
+];
+
+
 dibujarTodo();
 
-// =========================
-// FUNCIONES DE DIBUJO
-// =========================
+
 
 function limpiarCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-// PASO 5: Función dibujarTablero actualizada con rejilla completa
 function dibujarTablero() {
-  // Configuración estética: Línea tecnológica/cyberpunk (Cian sutil translúcido)
   ctx.strokeStyle = "rgba(56, 189, 248, 0.15)"; 
-  ctx.lineWidth = 1; // Grosor de línea fino y profesional
+  ctx.lineWidth = 1;
 
-  // PASO 9: Bucle 'for' para líneas verticales (Eje X)
   for (let x = 0; x <= canvas.width; x += TAMANIO_CELDA) {
     ctx.beginPath();
     ctx.moveTo(x, 0);              
@@ -30,20 +33,55 @@ function dibujarTablero() {
     ctx.stroke();                 
   }
 
-  // PASO 10: Bucle 'for' para líneas horizontales (Eje Y)
-  // Avanza en el eje Y de 25 en 25 píxeles ocupando todo el ancho (canvas.width)
   for (let y = 0; y <= canvas.height; y += TAMANIO_CELDA) {
     ctx.beginPath();
-    ctx.moveTo(0, y);             // Inicio izquierda: X en 0, Y actual
-    ctx.lineTo(canvas.width, y);  // Fin derecha: X al final del canvas, Y actual
-    ctx.stroke();                 // Dibuja la línea horizontal
+    ctx.moveTo(0, y);             
+    ctx.lineTo(canvas.width, y);  
+    ctx.stroke();                 
+  }
+}
+
+
+function pintarParte(lineaX, lineaY, esCabeza = false) {
+ 
+  const posX = lineaX * TAMANIO_CELDA;
+  const posY = lineaY * TAMANIO_CELDA;
+
+  
+  if (esCabeza) {
+    ctx.fillStyle = "#38bdf8";      
+    ctx.strokeStyle = "#ffffff";    
+  } else {
+    ctx.fillStyle = "#22c55e";      
+    ctx.strokeStyle = "#052e16";    
+  }
+
+ 
+  ctx.fillRect(posX, posY, TAMANIO_CELDA, TAMANIO_CELDA);
+  
+  
+  ctx.lineWidth = 2;
+  ctx.strokeRect(posX, posY, TAMANIO_CELDA, TAMANIO_CELDA);
+}
+
+
+function pintarSerpiente() {
+  
+  for (let i = 0; i < serpiente.length; i++) {
+    
+    const esCabeza = (i === 0);
+    
+    
+    pintarParte(serpiente[i].x, serpiente[i].y, esCabeza);
   }
 }
 
 function dibujarTodo() {
   limpiarCanvas();
-  // PASO 7: Invocar dibujarTablero desde dibujarTodo
   dibujarTablero();
+  
+  
+  pintarSerpiente();
 }
 
 
